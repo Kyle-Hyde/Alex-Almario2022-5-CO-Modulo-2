f15f39aa87711d03c5d9e8d30d20a426daa00a2f
Kyle-Hyde <120419081+Kyle-Hyde@users.noreply.github.com>
2022-12-14 23:29:27 -0500

    third_class

--- dino_runner/components/obstacles/birds.py
+++ dino_runner/components/obstacles/birds.py
@@ -1,31 +1,20 @@
-import pygame
-from pygame.sprite import Sprite
+import random
+
 from dino_runner.components.obstacles.obstacle import Obstacle
 from dino_runner.utils.constants import BIRD
-from  dino_runner.components.obstacles.obstacle_manager import obstacle_manager
 
-class bird(Obstacle):
-    def __init__(self):
-        self.image_b=BIRD[0]
-        self.bird_rect =self.image.get_rect()
-        self.bird_rect.y=200
-        self.step_index=0
-        self.bird_fly=True
+
+class Bird(Obstacle):
+  BIRD_HEIGHTS = [250, 220, 170]
+  def __init__(self):
+    self.type = 0
+    super().__init__(BIRD, self.type)
+    self.rect.y = self.BIRD_HEIGHTS[random.randint(0, 2)]
+    self.index = 0
     
-    def update(self):
-        if self.bird_fly:
-            self.fly()
-    
-    def fly(fly):
-        self.image_b= BIRD[0] if self.step_index < 5 else BIRD[1]
-        self.bird_rect =self.image.get_rect()
-        self.bird_rect.y =self.Y_POS
-        self.step_index -= 1
-
-    def draw(self,screen):
-        screen.blit(self.image, (self.bird_rect.x,self.bird_rect.y))
-   
-
-
-
-        
+  def draw(self, screen):
+    if self.index >= 9:
+     self.index = 0
+     
+    screen.blit(BIRD[self.index // 5], self.rect)
+    self.index += 1
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,28 +1,43 @@
 import pygame
+import random
+
+from dino_runner.utils.constants import SMALL_CACTUS
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.components.obstacles.cactus import Cactustwo
-from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
+from dino_runner.components.obstacles.birds import Bird
 
 
 class ObstacleManager:
     def __init__(self):
-        self.obstacles=[]
-
-    def update(self,game):
-        if len(self.obstacles)==0:
-            cactus= Cactus(SMALL_CACTUS)
+        self.obstacles = []
+    
+    def generate_obstacle(self, obstacle_type):
+        if obstacle_type == 0:
+            cactus_type = 'SMALL'
+            obstacle = Cactus(cactus_type)
+        elif obstacle_type == 1:
+            cactus_type = 'LARGE'
+            obstacle = Cactus(cactus_type)
+        else:
+             obstacle = Bird()
+        return obstacle
+    
+    def update(self, game):
+        if len(self.obstacles) == 0:
+            cactus = Cactus("SMALL")
             self.obstacles.append(cactus)
-            if len(self.obstacles)>=1:
-                cactus_two=Cactustwo(LARGE_CACTUS)
-                self.obstacles.append(cactus_two)
-            
+            obstacle_type = random.randint(0, 2)
+            obstacle = self.generate_obstacle(obstacle_type)
+            self.obstacles.append(obstacle)  
         for obstacle in self.obstacles:
-            obstacle.update(game.game_speed,self.obstacles)
+            obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
-                pygame.time.delay(10000)
+                pygame.time.delay(1000)
+                game.death_count+=1
                 game.playing=False
                 break
 
-    def draw(self,screen):
+    def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+    def reset_obstacles(self):
+        self.obstacles=[]
